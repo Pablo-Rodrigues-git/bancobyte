ecead17867b893ad174d7b301c951983a8f9a151
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-05-12 21:55:07 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/bancobyte/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bancobyte
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          const Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+          ),
+          const Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Column(
+            children: const [
+              Text(
+                "Craft beautiful UIs",
+                textDirection: TextDirection.ltr,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
