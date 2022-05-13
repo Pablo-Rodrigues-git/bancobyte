2718c4e37ad7699ecf26351e444127d1bf20b1ce
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-05-12 22:50:50 -0300

    extracting first method

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    body: Column(
+      home: Scaffold(
+        body: ListaTransferencias(),
+        appBar: AppBar(
+          title: Text("Transferências"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+      ),
+    ));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
       children: [
         Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('100.0'),
-            subtitle: Text('1000'),
-          )
-        ),Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('100.0'),
-            subtitle: Text('1000'),
-          )
-        ),Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('100.0'),
-            subtitle: Text('1000'),
-          )
-        ),Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('100.0'),
-            subtitle: Text('1000'),
-          )
-        ),
+            child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text('100.0'),
+          subtitle: Text('1000'),
+        )),
+        Card(
+            child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text('200.0'),
+          subtitle: Text('1000'),
+        )),
+        Card(
+            child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text('300.0'),
+          subtitle: Text('1000'),
+        )),
+        Card(
+            child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text('400.0'),
+          subtitle: Text('1000'),
+        )),Card(
+            child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text('500.0'),
+          subtitle: Text('1000'),
+        )),
       ],
-    ),
-    appBar: AppBar(title: Text("Transferências"),),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Icon(Icons.add),
-    ),
-  ),
-));
+    );
+  }
+}
